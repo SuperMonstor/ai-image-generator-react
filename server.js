c2d8a30f075be9e7d2659ecba097e2a4561c6e5f
Sudarshan S <senthilsudarshan@gmail.com>
2023-11-02 20:03:44 +0530

    Endpoint + Frontend call for images

--- server.js
+++ server.js
@@ -9,18 +9,26 @@ app.use(cors());
 app.use(express.json());
 require("dotenv").config; // to manage api keys
 
+// Open AI Package init
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 // Endpoint for images
 app.post("/images", async (req, res) => {
-	const { Configuration, OpenAIApi } = require("openai");
-	const configuration = new Configuration({
-		apiKey: process.env.OPENAI_API_KEY,
-	});
-	const openai = new OpenAIApi(configuration);
-	const response = await openai.createImage({
-		prompt: "A cute baby sea otter",
-		n: 2,
-		size: "1024x1024",
-	});
+	try {
+		const response = await openai.createImage({
+			prompt: "A cute baby sea otter",
+			n: 2,
+			size: "1024x1024",
+		});
+        console.log(response);
+        res.send(response.data.data);
+	} catch (error) {
+		console.error(error);
+	}
 });
 // Start server and listen for HTTP requests
 app.listen(PORT, () => console.log("Your server is running on PORT " + PORT));
--- src/App.js
+++ src/App.js
@@ -6,6 +6,29 @@ function App() {
 		"A girl citting over the edge of a city skyline smoking a cigarette. Sillouhette.",
 		"A pineapple sunbathing on an island",
 	];
+
+	const getImages = async () => {
+		try {
+			const options = {
+				method: "POST",
+				body: JSON.stringify({
+					message: "BLUGH",
+				}),
+				headers: {
+					"Content-type": "application/json",
+				},
+			};
+
+			const response = await fetch(
+				"http://localhost:8000/images",
+				options
+			);
+			const data = await response.json();
+			console.log(data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 	return (
 		<div className="App">
 			<section className="search-section">
